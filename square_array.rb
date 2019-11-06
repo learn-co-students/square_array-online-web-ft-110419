58835e48c29e5c271133f4bbea7344d740ec938c
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-06 15:22:47 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+# def square_array(array)
+# new_array=[]
+# counter=0
+# while counter<array.length do
+#   new_array<<array[counter]**2
+#   counter+=1
+# end
+# return new_array
+# end
